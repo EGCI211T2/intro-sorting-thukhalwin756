f2f4edbd96628ed9545f65dc87beb4a8b9e5b3ae
thukhalwin756 <thukha.lwi@student.mahidol.edu>
2026-01-31 11:15:09 +0000

    __

--- sorting.h
+++ sorting.h
@@ -1,3 +1,31 @@
 void swap(int &a,int &b);
-void sort(int *a,int N);
+void bubblesort(int *a,int N);
 void display(int *a,int N);
+
+void swap(int &a,int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+
+}
+
+void display(int a[],int N){
+    for (int i = 0; i < N; i++) {
+        cout << a[i] << " "<<endl;
+    }
+    
+}
+
+void bubblesort(int a[], int N){
+    int i,j;
+
+    for (i = N-1; i>0; i--) {
+        for (j = 0; j < i; j++) 
+	   { 
+        if(a[j]>a[j+1]) 
+        swap(a[j],a[j+1]);
+    }
+    }
+display(a,N);
+
+}
