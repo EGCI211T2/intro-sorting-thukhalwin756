fa1e962e0778974bc35561b9e4c8a3f5c2d3e53f
thukhalwin756 <thukha.lwi@student.mahidol.edu>
2026-01-31 11:14:19 +0000

    __

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 using namespace std;
+#include "sorting.h"
 
 int main(int argc, char * argv[]) {
     
@@ -20,13 +21,18 @@ int main(int argc, char * argv[]) {
     }
     /* Convert to numbers*/
     N=argc-1;
+    a = new int[N];
+    for (int i = 0; i < N; i++) {
+      a[i] = stoi(argv[i + 1]);
+    }
+    
     cout<<"Before sorting:";
     display(a,N);
     
-    sort(a,N);
+    bubblesort(a,N);
     cout<<"After sorting:";
     display(a,N);
     
-    
+    delete[] a;
     return 0;
 }
